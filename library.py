142704bcbc4f2be008685a24293fea61a333367a
Caca1234 <enn.truth@gmail.com>
2022-08-10 18:30:58 -0400

    Create library.py

---
+++ library.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def get(name):
+    c = requests.get('https://tikolu.net/edit/.text/' + name).text
+    return c
+
+def post(name, content):
+    values = {
+        'content': content,
+        'timestamp': None,
+        'ignoreconflict': True,
+    }
+    c = requests.post('https://tikolu.net/edit/' + name, json = values)
+    return c
