3ea003e6720af76063dffcd083502e11ca857c3b
Caca1234 <enn.truth@gmail.com>
2022-11-29 18:15:00 -0500

    Create ett.js

---
+++ js/ett.js
@@ -0,0 +1,40 @@
+ett = {
+    get: async function(page) {
+        return await fetch("https://tikolu.net/edit/.text/" + page).then(response => response.text());
+    },
+    post: async function(page, content) {
+        await fetch("https://tikolu.net/edit/" + page, {
+        method: "POST",
+        body: JSON.stringify({content: content, timestamp: 0, ignoreconflict: true})
+        });
+    }
+}
+
+class Virtual {
+    constructor(speed) {
+        this.pages = [];
+        this.speed = speed; // mbit/s
+    }
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async get(page) {
+        if (!this.pages[page]) {
+            content = ett.get(page);
+            this.pages[page] = content;
+	        
+            await sleep(content.length * this.speed);
+	        
+            return content;
+        }
+        content = this.pages[page];
+	    
+        await sleep(content.length * this.speed);
+	    
+        return content;
+    }
+    async post(page, content) {
+        await sleep(content.length * this.speed);
+        this.data[page] = content;
+    }
+}
